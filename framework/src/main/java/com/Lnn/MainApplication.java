9b85d504c2d7e3087e8bff9574aa1f0ac5d38aac
梁俊涛 <liangjt@landray.com.cn>
2024-07-19 21:03:57 +0800

    初始化环境，搭起springBoot

---
+++ framework/src/main/java/com/Lnn/MainApplication.java
@@ -0,0 +1,13 @@
+package com.Lnn;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
